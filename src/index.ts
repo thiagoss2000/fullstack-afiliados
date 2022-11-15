81844f0469a037736a4cffbccc089470dfebc64c
thiagoss2000 <thiago.ss675@gmail.com>
2022-11-15 10:26:06 -0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server on port ${port}`));
