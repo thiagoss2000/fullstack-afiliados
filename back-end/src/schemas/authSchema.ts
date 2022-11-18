d2ee206238ae5fa49e32379e691e67949afaea79
thiagoss2000 <thiago.ss675@gmail.com>
2022-11-18 18:52:32 -0300

    bugfix gitignore

--- back-end/src/schemas/authSchema.ts
+++
@@ -1,6 +0,0 @@
-import joi from "joi";
-
-export const authSchema = joi.object({
-  username: joi.string().min(6).required(),
-  password: joi.string().min(8).required()
-});
--- back-end/src/schemas/dataSchema.ts
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-export const dataSchema = joi.object({
-  type: joi.number().required(),
-  date: joi.date().iso().required(),
-  product: joi.string().required(),
-  price: joi.number().required(),
-  name: joi.string().required()
-});
